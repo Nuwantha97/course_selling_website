b78f9f8e6545f5b25419ce12bd57238c3a9e148e
Nuwantha97 <nuwanthasiriwardhana@gmail.com>
2024-12-08 12:45:03 +0530

    add route for dashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from './components/Dashboard';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/student/Home'; // Import the new Home component
 import './App.css';
 
 const App = () => {
   
   return (
     <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+      </Routes>
     </Router>
   );
 };
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from './Footer';
-import '../styles/Dashboard.css';
+import './Dashboard.css';
 
 const Dashboard = () => {
   return (
