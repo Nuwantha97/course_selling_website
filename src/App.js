544898fc9b060dd2378079cb6554a7cb54d9bf23
Nuwantha97 <nuwanthasiriwardhana@gmail.com>
2024-12-08 10:12:51 +0530

    reset

--- src/App.js
+++ src/App.js
@@ -7,52 +7,9 @@ import Home from './components/student/Home'; // Import the new Home component
 import './App.css';
 
 const App = () => {
-  const [showLoginModal, setShowLoginModal] = useState(false);
-  const [showRegisterModal, setShowRegisterModal] = useState(false);
-  const [popupMessage, setPopupMessage] = useState("");
-
-  const handleLoginSuccess = (username) => {
-    setPopupMessage(`Login as "${username}"`);
-    setTimeout(() => {
-      setPopupMessage(""); // Clear the popup after 2 seconds
-    }, 2000);
-  };
-
+  
   return (
     <Router>
-      <div className="App">
-        {/* Display Popup Message */}
-        {popupMessage && <div className="popup">{popupMessage}</div>}
-
-        {/* Routes for Dashboard and Home */}
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Dashboard
-                onShowLogin={() => setShowLoginModal(true)}
-                onShowRegister={() => setShowRegisterModal(true)}
-              />
-            }
-          />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-
-        {/* Modals for Login and Register */}
-        {showLoginModal && (
-          <div className="overlay">
-            <Login
-              onClose={() => setShowLoginModal(false)}
-              onLoginSuccess={handleLoginSuccess}
-            />
-          </div>
-        )}
-        {showRegisterModal && (
-          <div className="overlay">
-            <Register onClose={() => setShowRegisterModal(false)} />
-          </div>
-        )}
-      </div>
     </Router>
   );
 };
